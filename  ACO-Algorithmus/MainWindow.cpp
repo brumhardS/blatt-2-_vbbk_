e023739eab5077c769af39c8e67094703df5f050
Vasije Kastrati <vkastrat@hm.edu>
2017-05-01 14:50:21 +0200

    test

---  ACO-Algorithmus/MainWindow.cpp
+++  ACO-Algorithmus/MainWindow.cpp
@@ -1,11 +1,10 @@
 #include "MainWindow.h"
-#include "ui_mainwindow.h"
 
 
 MainWindow::MainWindow()
 {
     setWindowTitle("Ameisenkolonie");
-    setFixedSize(300,300);
+    setFixedSize(300,300);//
 }
 
 
